f7471a34422c2e8115d5543e861da8016e0edc68
beyoungjae <pz1998@naver.com>
2025-01-03 16:52:17 +0900

    up

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,8 +1,10 @@
+// nodemon 개발 모드 버그로 인해 routes 폴더 내의 middlewares 파일을 인식하지 못하여, server 폴더 내의 middlewares 파일로 이동
+
 exports.isLoggedIn = (req, res, next) => {
    if (req.isAuthenticated()) {
       next()
    } else {
-      res.status(403).json({ error: '로그인이 필요한 서비스입니다.' })
+      res.status(403).json({ success: false, message: '로그인이 필요합니다.' })
    }
 }
 
@@ -10,6 +12,6 @@ exports.isNotLoggedIn = (req, res, next) => {
    if (!req.isAuthenticated()) {
       next()
    } else {
-      res.status(403).json({ error: '이미 로그인한 사용자입니다.' })
+      res.status(403).json({ success: false, message: '이미 로그인이 된 상태입니다.' })
    }
 }
